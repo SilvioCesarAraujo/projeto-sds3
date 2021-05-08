883a1f081b0854f89914cdeeb25b108584908f9c
Silvio Araujo <araujo.scesar@gmail.com>
2021-05-08 15:42:43 -0300

    Pagination

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -25,13 +25,12 @@ const Dashboard = () => {
 
                 <div className="py-3">
                     <h2 className="text-primary">Todas Vendas</h2>
-
-
                 </div>
 
                 <DataTable />
 
             </div>
+
             <Footer />
         </>
     );
